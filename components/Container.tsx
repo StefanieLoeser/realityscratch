c411f0ef0a0acaceb29b89bde3ca877d21853c1a
StefanieLoeser <stefanie.loeser@gmx.net>
2023-02-20 15:23:25 +0100

    change styling

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,3 +1,5 @@
 export default function Container({ children }) {
-  return <div className="container mx-auto my-10 px-5">{children}</div>;
+  return (
+    <div className="container h-screen mx-auto my-10 px-5">{children}</div>
+  );
 }
--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -5,21 +5,23 @@ import Link from "next/link";
 function LandingPage() {
   return (
     <>
-      <Image
-        src={logoWithText}
-        alt="reality scratch logo: hand with long black finger nails"
-        className="py-20"
-      ></Image>
-      <div className="flex-wrap justify-center gap-15 my-20">
-        <p className="font-serif text-center text-xl mx-10">
-          A Berlin-based periodical for writers, artists, photographers,
-          musicians, magicians, vampires and general flâneurs.
-        </p>
-        <button className="m-20 p-5 text-white bg-black rounded-sm text-base">
-          <Link href="mailto:info@realityscratch.com">
-            Want in? Get in touch!
-          </Link>
-        </button>
+      <div className="flex-col items-center align-center">
+        <Image
+          src={logoWithText}
+          alt="reality scratch logo: hand with long black finger nails"
+          className="py-10"
+        ></Image>
+        <div className="flex flex-col gap-10 px-10">
+          <p className="font-serif text-center text-lg">
+            A Berlin-based periodical for writers, artists, photographers,
+            musicians, magicians, vampires and general flâneurs.
+          </p>
+          <button className="p-5 text-white bg-black rounded-sm text-sm">
+            <Link href="mailto:info@realityscratch.com">
+              Want in? Get in touch!
+            </Link>
+          </button>
+        </div>
       </div>
     </>
   );
